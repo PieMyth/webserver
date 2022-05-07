9186c69b691c822fc7f607c8ee48f34c2c3307ec
PieMyth <haugenw94@gmail.com>
2022-05-06 23:12:19 -0700

    Update libraries to more recent versions

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
-// Copyright © 2018 William Haugen - Piemyth
-// [This work is licensed under the "MIT License"]
-// Please see the file LICENSE in the source
-// distribution of this software for license terms.
+use std::{fs::File, io::{BufReader, Read}, path::Path, collections::HashMap};
 
-use actix_http::{body::Body, Response};
-use actix_web::dev::ServiceResponse;
-use actix_web::http::StatusCode;
-use actix_web::middleware::errhandlers::{ErrorHandlerResponse, ErrorHandlers};
-#[allow(unused_imports)]
-use actix_web::{error, middleware, web, App, Error, HttpResponse, HttpServer, Result};
-use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+use actix_web::{error, dev::{ServiceResponse, Service}, get, web, Error, http, App, HttpResponse, HttpServer, Result};
+use actix_web::{body::BoxBody, http::StatusCode};
+use actix_files::Files;
+#[allow(unused_imports)] // Added in case the logger is not used
+use actix_web::middleware::{Logger, ErrorHandlerResponse, ErrorHandlers};
+use futures_util::future::{self, Either, FutureExt};
+use rustls::{Certificate, PrivateKey, ServerConfig};
+use rustls_pemfile::{certs, pkcs8_private_keys};
 use tera::Tera;
 use serde::{Deserialize, Serialize};
-use std::fs::File;
-use actix_files as fs;
-use std::io::prelude::*;
-use std::path::Path;
-use std::collections::HashMap;
-use actix_web_middleware_redirect_https::RedirectHTTPS;
 
 //Structure that follows the json file
 #[derive(Serialize, Deserialize, Eq, Ord, PartialEq, PartialOrd)]
@@ -71,6 +63,7 @@ fn get_projects()-> Vec<Project> {
 // store tera template in application state
 // Gets the projects from the json file and hands them off to tera.
 // Renders the page afterwards.
+#[get("/")]
 async fn index(
     tmpl: web::Data<tera::Tera>
 ) -> Result<HttpResponse, Error> {
@@ -82,71 +75,99 @@ async fn index(
     Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
-#[actix_rt::main]
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=info");
-    env_logger::init();
-	
-	// Setup the cert files
-	// Comment out if you don't want https
-    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-	builder.set_private_key_file("./ssl/private.key.pem", SslFiletype::PEM).unwrap();
-    builder.set_certificate_chain_file("./ssl/domain.cert.pem").unwrap();
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-	// Creating the Actix HttpServer
+    // Setup the cert files
+	// Comment out if you don't want https
+    let cert_file = &mut BufReader::new(File::open("./ssl/domain.cert.pem").unwrap());
+    let key_file = &mut BufReader::new(File::open("./ssl/private.key.pem").unwrap());
+
+    let cert_chain: Vec<Certificate> = certs(cert_file)
+        .unwrap()
+        .into_iter()
+        .map(Certificate)
+        .collect();
+    let mut keys: Vec<PrivateKey> = pkcs8_private_keys(key_file)
+        .unwrap()
+        .into_iter()
+        .map(PrivateKey)
+        .collect();
+
+    let config = ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth()
+        .with_single_cert(cert_chain, keys.remove(0))
+        .unwrap();    
+
+    println!("starting HTTP server at http://localhost:8080");
+
     HttpServer::new(|| {
-		// Loads all of the html files into tera
-        let tera =
-            Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*.html")).unwrap();
+            // Loads all of the html files into tera
+            let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*.html")).unwrap();
 
         App::new()
-		// Loads Tera into the webapp
-            .data(tera)
-		// This will actively divert traffic from http (port 8080) to https (port 8443)
-		// Comment out if you intend to only support one way
-            .wrap(RedirectHTTPS::with_replacements(&[(":8080".to_owned(), ":8443".to_owned())]))
-		// Logger, showing status code of all get requests to the server as well as the time it took
-		// Comment out if you don't need it for debugging purposes
-            //.wrap(middleware::Logger::default()) // enable logger
-		// Tell the server where all of the static assets are
-            .service(fs::Files::new("/images", "./templates/images"))
-            .service(fs::Files::new("/css", "./templates/css"))
-            .service(fs::Files::new("/fonts", "./templates/fonts"))
-		// Tell the server where to route traffic to and which html file to use
-            .service(web::resource("/").route(web::get().to(index)))
-            .service(web::resource("/index.html").route(web::get().to(index)))
-		// Handle errors that the server may encounter
+            // Loads Tera into the webapp
+            .app_data(web::Data::new(tera))
+            // Logger, showing status code of all get requests to the server as well as the time it took
+		    // Comment out if you don't need it for debugging purposes
+            // .wrap(Logger::default()) // enable logger
+            // Tell the server where all of the static assets are
+            .service(Files::new("/images", "./templates/images"))
+            .service(Files::new("/css", "./templates/css"))
+            .service(Files::new("/fonts", "./templates/fonts"))
+            .service(index)
             .service(web::scope("").wrap(error_handlers()))
+            .wrap_fn(|sreq, srv| {
+                let host = sreq.connection_info().host().to_owned();
+                let uri = sreq.uri().to_owned();
+                let url = format!("https://{}{}", host, uri);
+
+                // If the scheme is "https" then it will let other services below this wrap_fn
+                // handle the request and if it's "http" then a response with redirect status code
+                // will be sent whose "location" header will be same as before, with just "http"
+                // changed to "https"
+                //
+                if sreq.connection_info().scheme() == "https" {
+                    Either::Left(srv.call(sreq).map(|res| res))
+                } else {
+                    return Either::Right(future::ready(Ok(sreq.into_response(
+                        HttpResponse::MovedPermanently()
+                            .append_header((http::header::LOCATION, url))
+                            .finish(),
+                    ))));
+                }
+            })
     })
-	//Open https socket
-    .bind_openssl("localhost:8443", builder)?
-	//Open http socket
-    .bind("localhost:8080")?
+    .bind(("localhost", 80))? // HTTP port
+    .bind_rustls(("localhost", 443), config)? // HTTPS port
     .run()
     .await
 }
 
-// Custom error handlers, to return HTML responses when an error occurs.
-fn error_handlers() -> ErrorHandlers<Body> {
+
+fn error_handlers() -> ErrorHandlers<BoxBody> {
     ErrorHandlers::new().handler(StatusCode::NOT_FOUND, not_found)
 }
 
 // Error handler for a 404 Page not found error.
-fn not_found<B>(res: ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
+fn not_found<B>(res: ServiceResponse<B>) -> Result<ErrorHandlerResponse<BoxBody>>  {
     let response = get_error_response(&res, "Page not found");
-    Ok(ErrorHandlerResponse::Response(
-        res.into_response(response.into_body()),
-    ))
+    Ok(ErrorHandlerResponse::Response(ServiceResponse::new(
+        res.into_parts().0,
+        response.map_into_left_body(),
+    )))
 }
 
 // Generic error handler.
-fn get_error_response<B>(res: &ServiceResponse<B>, error: &str) -> Response<Body> {
+fn get_error_response<B>(res: &ServiceResponse<B>, error: &str) -> HttpResponse {
     let request = res.request();
 
     // Provide a fallback to a simple plain text response in case an error occurs during the
     // rendering of the error page.
     let fallback = |e: &str| {
-        Response::build(res.status())
+        HttpResponse::build(res.status())
             .content_type("text/plain")
             .body(e.to_string())
     };
@@ -160,7 +181,7 @@ fn get_error_response<B>(res: &ServiceResponse<B>, error: &str) -> Response<Body
             let body = tera.render("error.html", &context);
 
             match body {
-                Ok(body) => Response::build(res.status())
+                Ok(body) => HttpResponse::build(res.status())
                     .content_type("text/html")
                     .body(body),
                 Err(_) => fallback(error),
